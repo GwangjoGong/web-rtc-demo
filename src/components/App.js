70c4db2b681dc0e6d471028b9f10d2dcf388ab3c
dwc05101 <dwc05101@kaist.ac.kr>
2021-01-12 15:54:57 +0900

    :sparkles: Implemented initial routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,23 @@
+import { Route, HashRouter as Router, Redirect } from "react-router-dom";
+import Lobby from "../routes/Lobby";
+import Login from "../routes/Login";
+import Room from "../routes/Room";
+
 function App() {
-  return <div>Hello, world!</div>;
+  return (
+    <Router>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/lobby">
+        <Lobby />
+      </Route>
+      <Route path="/room/:id">
+        <Room />
+      </Route>
+      <Redirect from="*" to="/login"></Redirect>
+    </Router>
+  );
 }
 
 export default App;
