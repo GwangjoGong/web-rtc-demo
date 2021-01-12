4c1c5cc4f2acf4445ca6cf898a2e795d13db6d57
dwc05101 <dwc05101@kaist.ac.kr>
2021-01-13 00:32:14 +0900

    :sparkles: Updated api endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `https://dm-rtc-demo-server.herokuapp.com:4000`,
+  baseURL: `https://dm-rtc-demo-server.herokuapp.com`,
 });
 
 export const getToken = () => {
