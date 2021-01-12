9b0c80bbb192917edc4552cf6b415ca54f6d7474
dwc05101 <dwc05101@kaist.ac.kr>
2021-01-13 00:20:59 +0900

    :sparkles: Updated api endpoint

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: "http://localhost:4000" });
+const axiosInstance = axios.create({ baseURL: "http://54.180.211.107:4000" });
 
 export const getToken = () => {
   return axiosInstance.get("/token");
